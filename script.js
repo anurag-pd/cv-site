06bbbcc75f91c519eeb75e8e246dd32e245ceae2
Anurag <92098274+anurag-pd@users.noreply.github.com>
2022-04-14 13:13:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+    $(window).scroll(function(){
+        var sT = $(window).scrollTop();
+        if(sT>700){
+            $("#topScroll").fadeIn(250);
+        }
+        else{
+            $("#topScroll").fadeOut(250);
+        }
+    });
+
+    $("#topScroll").click(function(){
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+});
