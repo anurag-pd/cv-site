6ce387dd63fa1eb8aa447fd2fb1b6c033f4259e8
anurag <prasad.anurag.ap@gmail.com>
2025-06-08 02:24:40 +0530

    added thora tonned down

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-$(function(){
-    $(window).scroll(function(){
-        var sT = $(window).scrollTop();
-        if(sT>700){
-            $("#topScroll").fadeIn(250);
-        }
-        else{
-            $("#topScroll").fadeOut(250);
-        }
-    });
+$(function () {
+  $(window).scroll(function () {
+    var sT = $(window).scrollTop();
+    if (sT > 700) {
+      $("#topScroll").fadeIn(250);
+    } else {
+      $("#topScroll").fadeOut(250);
+    }
+  });
 
-    $("#topScroll").click(function(){
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
+  $("#topScroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
 });
